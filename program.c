c12d7097f854b1d0bca35c833049d70d87b6ec39
Kendyn Fredieu <kendyn.fredieu@gmail.com>
2025-09-09 15:30:27 -0400

    Complete exercise 3-4

--- program.c
+++ program.c
@@ -2,14 +2,16 @@
 #include <unistd.h>
 
 int main() {
-    pid_t pid = fork();
+    pid_t pid1 = fork();
 
-    if (pid > 0) {
+    if (pid1 > 0) {
         printf("I am the parent, PID: %d\n", getpid());
-    } else if (pid == 0) {
+    } else if (pid1 == 0) {
         printf("I am the child, PID: %d\n", getpid());
-    } else {
-        printf("Fork failed!\n");
+        pid_t pid2 = fork();
+        if (pid2 == 0) {
+            printf("I am the grandchild, PID: %d\n", getpid());
+        }
     }
     return 0;
 }
