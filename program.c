7deeaa295011c96a964aa2e44a7ce3741465e4aa
Kendyn Fredieu <kendyn.fredieu@gmail.com>
2025-09-09 15:07:40 -0400

    completed program.c

---
+++ program.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+    pid_t pid = fork();
+
+    if (pid > 0) {
+        printf("I am the parent, PID: %d\n", getpid());
+    } else if (pid == 0) {
+        printf("I am the child, PID: %d\n", getpid());
+    } else {
+        printf("Fork failed!\n");
+    }
+    return 0;
+}
